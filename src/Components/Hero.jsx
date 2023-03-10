b5cf7785fdffa94a74a0be686701d4e003f40d9f
alit <alitwiradanta08@gmail.com>
2023-03-10 16:00:01 +0800

    add Navbar and Hero Section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,28 @@
+import "../index.css"
+import desktopImage from "../Image/Desktop.svg"
+
+
 const Hero = () => {
     return(
 
-        <h2></h2>
+        <div style={{height:'100vh'}} className="desktopImg fContainer w-100 d-flex justify-center align-items-center">
+            <div className="text-center">
+                <h2 style={{color:'white', fontSize:'64px' , fontWeight:'700'}}>Discover Your Life, Travel <br /> Where You Want</h2>
+                <p className="mt-3" style={{color:'white', fontSize:'24px' , fontWeight:'300'}}>Would you explore natur paradise in the world, let’s find the best <br /> destination in world with us.</p>
+
+                {/* Buttons */}
+                <div className="d-flex justify-center mt-4">
+                <button class="HeroButton"> Lihat Selengkapnya
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+                    </div>
+                </button>                                                                                                                                                            
+                </div>
+
+
+            </div>
+        </div>
+
     )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,24 +2,38 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+window.onscroll = function() {
+        scrollFunction()
+    };
 
+    function scrollFunction() {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbarStyle").style.backgroundColor ="rgba( 62, 134, 245, 0.15 )";
+            document.getElementById("navbarStyle").style.backdropFilter ="blur( 8.5px )";
+            document.getElementById("navbarStyle").style.transition="0.5s";
+          } else {
+            document.getElementById("navbarStyle").style.backgroundColor ="transparent";
+            document.getElementById("navbarStyle").style.backdropFilter ="blur( 8.5px )";
+            document.getElementById("navbarStyle").style.transition="0.5s";
+        }
+    }
 
 
 
 const Navigation = () => {
     return(
 
-        <Navbar id='navbarStyle' bg="dark" fixed='top' expand="lg">
+        <Navbar id='navbarStyle' fixed='top' expand="lg">
         <Container>
-          <Navbar.Brand style={{color:'white'}} href="#home">Travelo</Navbar.Brand>
+          <Navbar.Brand style={{color:'white', fontSize:'24px'}} href="#home">Travelo</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className='text-center'>
-            <Nav className='text-center w-100 d-flex justify-end'>
-              <Nav.Link style={{color:'white'}} className="me-2" href="#home">Review</Nav.Link>
-              <Nav.Link style={{color:'white'}} className="me-2" href="#link">Tips</Nav.Link>
-              <Nav.Link style={{color:'white'}} className="me-2" href="#link">Alerts</Nav.Link>
-              <Nav.Link style={{color:'white'}} className="me-2" href="#link">Blog</Nav.Link>
-              <Nav.Link style={{color:'white'}} className="me-2" href="#link">Login</Nav.Link>
+            <Nav className='text-center w-100 d-flex justify-end' style={{fontSize:'18px'}}>
+              <Nav.Link id='hoverNavLink' className="me-3"href="#home">Review</Nav.Link>
+              <Nav.Link id='hoverNavLink' className="me-3" href="#link">Tips</Nav.Link>
+              <Nav.Link id='hoverNavLink' className="me-3" href="#link">Alerts</Nav.Link>
+              <Nav.Link id='hoverNavLink' className="me-3" href="#link">Blog</Nav.Link>
+              <Nav.Link id='hoverNavLink' className="me-3" href="#link">Login</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
